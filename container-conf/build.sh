93bd3eab75b82402e6699b524df8d998ff21951b
robnagler <github@q33.us>
2017-08-07 23:55:33 +0000

    move OPAL to part2 for faster testing

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -4,5 +4,5 @@ build_travis_trigger_next=( beamsim )
 
 build_as_run_user() {
     cd "$build_guest_conf"
-    curl radia.run | codes_dir="$(pwd)/codes" bash -s code synergia elegant genesis shadow3
+    curl radia.run | codes_dir="$(pwd)/codes" bash -s code pyOPALTools trilinos opal
 }
