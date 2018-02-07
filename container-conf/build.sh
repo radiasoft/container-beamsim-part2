7cf0a6faa1c2b0032d7bc02412a3d520018aea3f
robnagler <github@q33.us>
2018-02-07 16:25:24 +0000

    try opal-9 again

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -5,6 +5,5 @@ build_is_public=1
 
 build_as_run_user() {
     cd "$build_guest_conf"
-    # opal-1.9 is failing so do nothing for now
-    #install_repo_eval code pyOPALTools trilinos opal
+    install_repo_eval code pyOPALTools trilinos opal
 }
